0a3ac3b763857c02dfe3716f2b58a4cfa1027667
emilypo89 <emilypo89@gmail.com>
2017-07-10 12:03:56 -0400

    models

---
+++ models/todo.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, DataTypes) {
+  var ToDo = sequelize.define("ToDo", {
+      toDo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  },
+    // Here we'll pass a second "classMethods" object into the define method
+    // This is for any additional configuration we want to give our models
+    {
+      // We're saying that we want our User to have todo
+      classMethods: {
+        associate: function(models) {
+          // Associating User with Todos
+          // When an User is deleted, also delete any associated Todo
+          ToDo.belongsTo(models.User, {
+            onDelete: "cascade"
+          });
+        }
+      }
+    }
+  );
+  return ToDo;
+};
