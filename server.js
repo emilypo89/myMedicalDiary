6457de5eae7e0b934b7a0d2fdab133219c488f2a
emilypo89 <emilypo89@gmail.com>
2017-07-11 20:26:18 -0400

    creates doctor

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.set("view engine", "handlebars");
 // =============================================================
 // // Import routes and give the server access to them.
 
-var routes = require("./routes/appointmentRoutes.js");
+var routes = require("./routes/doctorRoutes.js");
 
 
 app.use("/", routes);
