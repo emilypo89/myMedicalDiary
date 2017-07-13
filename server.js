8864cc513fcf329daeca53a6d6d525a60ce58e44
emilypo89 <emilypo89@gmail.com>
2017-07-13 13:01:58 -0400

    mmd routes cleaned up

--- server.js
+++ server.js
@@ -7,42 +7,39 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+// var isAuthenticated = require("./config/middleware/isAuthenticated");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 // Requiring our models for syncing
 var db = require("./models");
-
 // Sets up the Express app to handle data parsing
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
+// app.use(bodyParser.json({ type: “application/vnd.api+json” }));
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
-
 // Static directory - give you access to the stuff in the public folder
 app.use(express.static("public"));
-
 // Set Handlebars.
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
 // Routes - NEED TO ADD ROUTES!!!!
 // =============================================================
 // // Import routes and give the server access to them.
-
 var routes = require("./routes/mmdRoutes.js");
-
-
 app.use("/", routes);
-
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({force: false}).then(function() {
@@ -50,5 +47,3 @@ db.sequelize.sync({force: false}).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-
